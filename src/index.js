b4539d71f7749d371768635ef35a03afc181fc6c
Iamdkj@237 <dhanesh4u237@gmail.com>
2025-06-01 15:51:37 +0530

    google manager

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router';
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-3FD5CP6E10");
+
+// Send pageview with a custom path
+ReactGA.send({ hitType: "pageview", page: "/my-path", title: "Custom Title" });
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
