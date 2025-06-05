935b05eb9ce3b75623424fc696d4ba80b689ddae
Your Name <you@example.com>
2025-06-05 21:53:10 +0530

    changes

--- menu/tailwind.config.js
+++ menu/tailwind.config.js
@@ -1,10 +1,46 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+const plugin = require('tailwindcss/plugin');
+
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
+    "public/**/*.html"
   ],
+  darkMode: 'class',  // Enable dark mode using the 'dark' class
   theme: {
-    extend: {},
+    extend: {
+     
+    },
   },
-  plugins: [],
-}
+  variants: {
+    backgroundColor: [
+      'hover',
+      'focus',
+      'active',
+      'odd',
+      'dark',
+      'dark:hover',
+      'dark:focus',
+      'dark:active',
+      'dark:odd',
+    ],
+    display: ['responsive', 'dark'],
+    textColor: [
+      'focus-within',
+      'hover',
+      'active',
+      'dark',
+      'dark:focus-within',
+      'dark:hover',
+      'dark:active',
+    ],
+    placeholderColor: ['focus', 'dark', 'dark:focus'],
+    borderColor: ['focus', 'hover', 'dark', 'dark:focus', 'dark:hover'],
+    divideColor: ['dark'],
+    boxShadow: ['focus', 'dark:focus'],
+  },
+  
+};
