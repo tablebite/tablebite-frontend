14deab33e6634b8f87232b97edadee085ca20368
Your Name <you@example.com>
2025-06-11 00:41:19 +0530

    df

--- menu/src/Super-admin/Sidebar.jsx
+++ menu/src/Super-admin/Sidebar.jsx
@@ -81,7 +81,7 @@ export default function Sidebar({
   return (
     <>
       {/* Desktop Sidebar */}
-      <aside className="z-20 hidden w-64 overflow-y-auto bg-white dark:bg-gray-800 md:block">
+      <aside className="z-20 hidden w-64  shadow-sm overflow-y-auto bg-white dark:bg-gray-800 md:block">
         <div className="py-4 text-gray-500 dark:text-gray-400">
           <a className="ml-6 text-lg font-bold text-gray-800 dark:text-gray-200" href="#">
             Tablebite
