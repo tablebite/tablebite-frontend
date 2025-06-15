d3382635a760a3f7231bad586bb16e03241c663b
Your Name <you@example.com>
2025-06-15 20:34:22 +0530

    sdf

--- menu/src/views/admin/profile/components/Banner.jsx
+++ menu/src/views/admin/profile/components/Banner.jsx
@@ -19,7 +19,7 @@ const Banner = () => {
       {/* Name and position */}
       <div className="mt-16 flex flex-col items-center">
         <h4 className="text-xl font-bold text-navy-700 dark:text-white">
-          Adela Parkson
+         Dhanesh KJ
         </h4>
         <p className="text-base font-normal text-gray-600">Product Manager</p>
       </div>
