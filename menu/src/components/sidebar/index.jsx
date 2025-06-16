e7f8ce020c06796aefba0d1c94de01c470b410ba
Your Name <you@example.com>
2025-06-16 22:55:39 +0530

    ch

--- menu/src/components/sidebar/index.jsx
+++ menu/src/components/sidebar/index.jsx
@@ -1,17 +1,14 @@
 /* eslint-disable */
-
 import { HiX } from "react-icons/hi";
 import Links from "./components/Links";
-
 import SidebarCard from "components/sidebar/componentsrtl/SidebarCard";
 import routes from "routes.js";
 
 const Sidebar = ({ open, onClose }) => {
   return (
     <div
-      className={`sm:none duration-175 linear fixed !z-50 flex min-h-full flex-col bg-white pb-10 shadow-2xl shadow-white/5 transition-all dark:!bg-navy-800 dark:text-white md:!z-50 lg:!z-50 xl:!z-0 ${
-        open ? "translate-x-0" : "-translate-x-96"
-      }`}
+      className={`sm:none duration-175 linear fixed min-h-full flex flex-col bg-white pb-10 shadow-2xl shadow-white/5 transition-all dark:!bg-navy-800 dark:text-white md:!z-50 lg:!z-50 xl:!z-0 
+      xl:z-10 lg:z-10 md:z-10 z-[9999] ${open ? "translate-x-0" : "-translate-x-96"}`} // Make sure the sidebar has the highest z-index
     >
       <span
         className="absolute top-4 right-4 block cursor-pointer xl:hidden"
@@ -21,11 +18,11 @@ const Sidebar = ({ open, onClose }) => {
       </span>
 
       <div className={`mx-[56px] mt-[50px] flex items-center`}>
-        <div className="mt-1 ml-1 h-2.5 font-poppins text-[26px] font-bold  text-navy-700 dark:text-white">
-          Tablebite <span class="font-medium"></span>
+        <div className="mt-1 ml-1 h-2.5 font-poppins text-[26px] font-bold text-navy-700 dark:text-white">
+          Tablebite <span className="font-medium"></span>
         </div>
       </div>
-      <div class="mt-[58px] mb-7 h-px bg-gray-300 dark:bg-white/30" />
+      <div className="mt-[58px] mb-7 h-px bg-gray-300 dark:bg-white/30" />
       {/* Nav item */}
 
       <ul className="mb-auto pt-1">
